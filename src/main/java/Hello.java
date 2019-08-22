bef37da3e01eb128e19e3edd927b69322cbc2c29
as49537023 <as49537023@hotmail.com>
2019-08-23 03:36:31 +0800

    new

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+import com.joba.Person;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+            Person p = new Person();
+            p.hello();
+    }
+}
---
+++ src/main/kotlin/com/joba/Hello.kt
@@ -0,0 +1,16 @@
+package com.joba
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+        val h = Human()
+        h.hello()
+
+
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
